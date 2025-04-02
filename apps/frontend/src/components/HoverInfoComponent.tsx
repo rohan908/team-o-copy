fbd8d119dab2314387413234fed75193468e1dae
Mario-Runner <73408352+Mario-Runner@users.noreply.github.com>
2025-04-02 19:56:56 -0400

    Updated with maps from Camden and transparency is now respected by the area labels. They will only indicate you're hovering over a colored area if you're actually on a colored area, not a nearby white area.

--- apps/frontend/src/components/HoverInfoComponent.tsx
+++ apps/frontend/src/components/HoverInfoComponent.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect, useRef, MouseEvent, MouseEventHandler} from 'react';
 import {useState} from 'react';
 import '../routes/MapPage/MapPageStyles.css';
+import {Simulate} from "react-dom/test-utils";
+import mouseOver = Simulate.mouseOver;
 
 const HoverInfoComponent = () => {
     const [hoverInfo, setHoverInfo] = useState("");
     const [isHovering, setIsHovering] = useState(false);
+    const [isOverTransparent, setIsOverTransparent] = useState(false);
+    const canvasRefImaging = useRef<HTMLCanvasElement>(null);
+    const canvasRefPharmacy = useRef<HTMLCanvasElement>(null);
+    const canvasRefPhlebotomy = useRef<HTMLCanvasElement>(null);
+    const canvasRefSpecClinic = useRef<HTMLCanvasElement>(null);
+    const canvasRefUrgentCare = useRef<HTMLCanvasElement>(null);
+
     function handleMouseEnter(num: number) {
         //Has cases for each area.
         //The area tag should supply which area number it is and match it to the correct description
@@ -40,51 +49,136 @@ const HoverInfoComponent = () => {
         setIsHovering(false);
     }
 
+    function handleMouseOver(e: MouseEvent, num: number) {
+        let canvas;
+        let context
+        switch (num) {
+            case 2:
+                canvas = canvasRefImaging.current;
+                if (!canvas) return;
+                context = canvas.getContext("2d");
+                if (!context) return;
+                break;
+            case 3:
+                canvas = canvasRefPharmacy.current;
+                if (!canvas) return;
+                context = canvas.getContext("2d");
+                if (!context) return;
+                break;
+            case 4:
+                canvas = canvasRefPhlebotomy.current;
+                if (!canvas) return;
+                context = canvas.getContext("2d");
+                if (!context) return;
+                break;
+            case 5:
+                canvas = canvasRefSpecClinic.current;
+                if (!canvas) return;
+                context = canvas.getContext("2d");
+                if (!context) return;
+                break;
+            case 6:
+                canvas = canvasRefUrgentCare.current;
+                if (!canvas) return;
+                context = canvas.getContext("2d");
+                if (!context) return;
+                break;
+        }
+        if (!canvas) return;
+        if (!context) return;
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const pixelData = context.getImageData(x,y,1,1);
+        console.log("Alpha value = " + pixelData.data[3]);
+        if (pixelData.data[3] == 0){
+            setIsOverTransparent(true);
+        } else {
+            setIsOverTransparent(false);
+        }
+    }
+
+    useEffect(() => {
+        let canvas = canvasRefImaging.current;
+        if (!canvas) return;
+        let context = canvas.getContext('2d');
+        if (!context) return;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        let image = new Image();
+        image.src = "public/MapImages/ImagingSuiteAlphaChannel.png";
+        context.beginPath();
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        canvas = canvasRefPharmacy.current;
+        if (!canvas) return;
+        context = canvas.getContext('2d');
+        if (!context) return;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        image = new Image();
+        image.src = "public/MapImages/Pharmacy.png";
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        canvas = canvasRefPhlebotomy.current;
+        if (!canvas) return;
+        context = canvas.getContext('2d');
+        if (!context) return;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        image = new Image();
+        image.src = "public/MapImages/Phlebotomy.png";
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        canvas = canvasRefSpecClinic.current;
+        if (!canvas) return;
+        context = canvas.getContext('2d');
+        if (!context) return;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        image = new Image();
+        image.src = "public/MapImages/SpecClinic.png";
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        canvas = canvasRefUrgentCare.current;
+        if (!canvas) return;
+        context = canvas.getContext('2d');
+        if (!context) return;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        image = new Image();
+        image.src = "public/MapImages/UrgentCare.png";
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    })
+
     return (
         <div>
             <div className="bg-gray-100 rounded-lg shadow-md p-6 w-300">
                 <div className="text-2xl font-bold mb-4 text-center py-2 rounded">
-                    {!isHovering ? `No information to display` : `Area Information: ${hoverInfo}`}
+                    {!isHovering || isOverTransparent ? `No information to display` : `Area Information: ${hoverInfo}`}
                 </div>
             </div>
             <div className="mapContainer">
-                <img src="public/MapImages/Rough_Floor_Plan.png" alt="MainMapImage" className = "mainMapImage"/>
-                <img src="public/MapImages/ImagingSuite.png" alt="MainMapImage"  className = "imagingSuite"
-                     onMouseEnter={() => handleMouseEnter(2)}
-                     onMouseLeave={() => handleMouseExit()}/>
-                <img src="public/MapImages/Pharmacy.png" alt="MainMapImage" className = "pharmacy"
-                     onMouseEnter={() => handleMouseEnter(3)}
-                     onMouseLeave={() => handleMouseExit()}/>
-                <img src="public/MapImages/Phlebotomy.png" alt="MainMapImage" className = "phlebotomy"
-                     onMouseEnter={() => handleMouseEnter(4)}
-                     onMouseLeave={() => handleMouseExit()}/>
-                <img src="public/MapImages/SpecClinic.png" alt="MainMapImage" className = "specClinic"
-                     onMouseEnter={() => handleMouseEnter(5)}
-                     onMouseLeave={() => handleMouseExit()}/>
-                <img src="public/MapImages/UrgentCare.png" alt="MainMapImage" className = "urgentCare"
-                     onMouseEnter={() => handleMouseEnter(6)}
-                     onMouseLeave={() => handleMouseExit()}/>
-            </div>
-            <div
-                className="bg-gray-300 rounded-lg shadow-md p-6 w-64"
-                onMouseEnter={() => handleMouseEnter(1)}
-                onMouseLeave={() => handleMouseExit()}
-            >
-                <div className="text-2xl font-bold mb-4 text-center py-2 rounded">{`Area 1`}</div>
-            </div>
-            <div
-                className="bg-gray-400 rounded-lg shadow-md p-6 w-64"
-                onMouseEnter={() => handleMouseEnter(2)}
-                onMouseLeave={() => handleMouseExit()}
-            >
-                <div className="text-2xl font-bold mb-4 text-center py-2 rounded">{`Area 2`}</div>
-            </div>
-            <div
-                className="bg-gray-500 rounded-lg shadow-md p-6 w-64"
-                onMouseEnter={() => handleMouseEnter(3)}
-                onMouseLeave={() => handleMouseExit()}
-            >
-                <div className="text-2xl font-bold mb-4 text-center py-2 rounded">{`Area 3`}</div>
+                <img
+                    src="public/MapImages/Whole_Floor_Plan.png"
+                    alt="MainMapImage"
+                    className="mainMapImage"
+                />
+                <canvas id="imaging" className="imagingSuite" width={479} height={520} ref={canvasRefImaging}
+                        onMouseEnter={() => handleMouseEnter(2)}
+                        onMouseLeave={() => handleMouseExit()}
+                        onMouseMove={(event) => handleMouseOver(event, 2)}/>
+                <canvas id="specClinic" className="specClinic" width={474} height={532} ref={canvasRefSpecClinic}
+                        onMouseEnter={() => handleMouseEnter(5)}
+                        onMouseLeave={() => handleMouseExit()}
+                        onMouseMove={(event) => handleMouseOver(event, 5)}/>
+                <canvas id="urgentCare" className="urgentCare" width={273} height={229} ref={canvasRefUrgentCare}
+                        onMouseEnter={() => handleMouseEnter(6)}
+                        onMouseLeave={() => handleMouseExit()}
+                        onMouseMove={(event) => handleMouseOver(event, 6)}/>
+                <canvas id="pharmacy" className="pharmacy" width={49} height={96} ref={canvasRefPharmacy}
+                        onMouseEnter={() => handleMouseEnter(3)}
+                        onMouseLeave={() => handleMouseExit()}
+                        onMouseMove={(event) => handleMouseOver(event, 3)}/>
+                <canvas id="phlebotomy" className="phlebotomy" width={82} height={152} ref={canvasRefPhlebotomy}
+                        onMouseEnter={() => handleMouseEnter(4)}
+                        onMouseLeave={() => handleMouseExit()}
+                        onMouseMove={(event) => handleMouseOver(event, 4)}/>
             </div>
         </div>
     );
