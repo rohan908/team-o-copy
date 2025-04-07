31d5ace2c4c569b2975aad9a12a83b44e86abe5a
lhoda <lhodriscoll@wpi.edu>
2025-04-07 10:36:41 -0400

    Cleaned up logic for checking login

--- apps/frontend/src/home-page/components/nav-bar.tsx
+++ apps/frontend/src/home-page/components/nav-bar.tsx
@@ -1,19 +1,18 @@
-import { Outlet, Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Outlet, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import "../home-style.css"
-import { useState, useEffect } from "react";
-import LoginPage from "../../login-components/login-page.tsx";
-import Service from "../../service-request/service.tsx";
+import '../home-style.css';
+import { useState, useEffect } from 'react';
+import LoginPage from '../../login-components/login-page.tsx';
+import Service from '../../service-request/service.tsx';
 
 export function NavBar() {
-
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
     useEffect(() => {
         const checkLoginStatus = () => {
-            const username = localStorage.getItem("username");
-            const password = localStorage.getItem("password");
+            const username = localStorage.getItem('username');
+            const password = localStorage.getItem('password');
             setIsLoggedIn(!!username && !!password);
         };
         checkLoginStatus();
@@ -23,37 +22,43 @@ export function NavBar() {
             window.removeEventListener('storage', checkLoginStatus);
         };
     }, []);
-        useEffect(() => {
-            if (!isLoggedIn) {
-                //navigate("/");
-            }
-        }, [isLoggedIn]);
-        return (
+    useEffect(() => {
+        if (!isLoggedIn) {
+            //navigate("/");
+        }
+    }, [isLoggedIn]);
+    return (
         <>
-            <nav className={"bg-blue-500"}>
-                <ul className={"flex place-items-center place-content-start space-x-5 h-20"}>
-                    <li className={"nav-element font-bold w-20 text-xl"}>
+            <nav className={'bg-blue-500'}>
+                <ul className={'flex place-items-center place-content-start space-x-5 h-20'}>
+                    <li className={'nav-element font-bold w-20 text-xl'}>
                         <Link to="/">
-                            <img className={"rounded hover-shadow"} src={"public/logo.png"} alt={"Home"} width={"100%"} height={"100%"}/>
+                            <img
+                                className={'rounded hover-shadow'}
+                                src={'public/logo.png'}
+                                alt={'Home'}
+                                width={'100%'}
+                                height={'100%'}
+                            />
                         </Link>
                     </li>
-                    <li className={"nav-element"}>
+                    <li className={'nav-element'}>
                         <Link to="/map-page">Map</Link>
                     </li>
-                    <li className={"nav-element"}>
+                    <li className={'nav-element'}>
                         <Link to="/directory">Directory</Link>
                     </li>
                     {isLoggedIn && (
-                        <li className={"nav-element"}>
-                            <Service />
-                        </li>
+                        <>
+                            <li className={"nav-element"}>
+                                <Link to="/DisplayRequestsPage">Display Requests Page</Link>
+                            </li>
+                            <li className={'nav-element'}>
+                                <Service />
+                            </li>
+                        </>
                     )}
-                    {isLoggedIn && (
-                        <li className={"nav-element"}>
-                            <Link to="/DisplayRequestsPage">Display Requests Page</Link>
-                        </li>
-                    )}
-                    <li className={"nav-element"}>
+                    <li className={'nav-element'}>
                         <LoginPage setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
                     </li>
                 </ul>
