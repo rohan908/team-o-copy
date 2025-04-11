76d06ff061587f7c6d4f6cc20fe6753ab98c8081
rohan908 <145878828+rohan908@users.noreply.github.com>
2025-04-11 18:07:20 -0400

    map texture on plane done, fixing container css display issues

--- apps/frontend/src/IndoorMapPage/DraggableMap.tsx
+++ apps/frontend/src/IndoorMapPage/DraggableMap.tsx
@@ -1,9 +1,12 @@
 import {useEffect} from 'react';
 import * as THREE from "three";
 import Compass from "threejs-compass";
-import {Container} from "@mantine/core";
+import {Container, useMantineTheme} from "@mantine/core";
+import Stats from "three/examples/jsm/libs/stats.module.js"
+
 
 export function DraggableMap() {
+  const theme = useMantineTheme();
   useEffect(() => {
     const scene = new THREE.Scene();
     const canvas = document.getElementById('insideMapCanvas');
@@ -28,7 +31,7 @@ export function DraggableMap() {
     const compass : Compass = new Compass(camera, renderer);
     compass.setAllEvents();
 
-    scene.background = new THREE.Color(0x000000);
+    scene.background = new THREE.Color(theme.colors.blueBase[9]);
     scene.fog = new THREE.Fog(0x000000, 96, 1000);
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -40,15 +43,30 @@ export function DraggableMap() {
     spotLight.position.set(0, 64, 32);
     scene.add(spotLight);
 
+    /*
     const boxGeo = new THREE.BoxGeometry(16, 16, 16);
     const boxMat = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeo, boxMat);
     scene.add(boxMesh);
+    */
+
+    const mapTexture = new THREE.TextureLoader().load("/public/MapImages/OutsideMap.png");
+    const mapGeo = new THREE.PlaneGeometry(500, 400);
+    const mapMaterial = new THREE.MeshBasicMaterial({map: mapTexture});
+    const mapPlane = new THREE.Mesh(mapGeo, mapMaterial);
+    scene.add(mapPlane);
+
+    //you can get rid of this later but its for debugging to see that im not eating a huge amount into the browser lol, shows stats in the top left
+    const stats = Stats();
+    document.body.appendChild(stats.domElement);
 
     const animate = () => {
+      /*
       boxMesh.rotation.x += 0.01;
       boxMesh.rotation.y += 0.01;
-      
+      */
+
+      stats.update();
       renderer.render(scene, camera);
       window.requestAnimationFrame(animate);
     };
@@ -57,8 +75,8 @@ export function DraggableMap() {
   }, []);
 
   return (
-    <Container width="100%" height="100%">
-      <canvas id="insideMapCanvas" style={{width: "100%", height: "100%"}}/>
+    <Container w="100%" h="100%">
+      <canvas id="insideMapCanvas" style={{width: "100vw", height: "100vh"}}/>
     </Container>
   )
 
