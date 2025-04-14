83dc788cdeec197c16159a4690324a97898efd1f
Owen Hart <owenvhart@gmail.com>
2025-04-14 13:41:48 -0400

    fixes pathfind post request, error handlers need to be looked at

--- apps/backend/src/routes/Graph.ts
+++ apps/backend/src/routes/Graph.ts
@@ -69,9 +69,9 @@ const findPathHandler: RequestHandler<
             });
             return; // Return void
         }
-        res.json({
-            result,
-        });
+
+        res.send({ result });
+
         return; // Return void explicitly
     } catch (error) {
         const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
@@ -88,7 +88,7 @@ const findPathHandler: RequestHandler<
 // Debug endpoint to get test the pathfinding between nodes
 router.get('/debug', (req: any, res: any) => {
     // Get the grid dimensions and some sample walkable points
-    const path = navigationService.findPath(1, 5, "BFS");
+    const path = navigationService.findPath(1, 5, 'BFS');
     res.json(path);
 });
 
