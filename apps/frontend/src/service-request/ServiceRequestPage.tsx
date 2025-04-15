3ab56e038cc2d364a6c5e65639fce00e8c18ba59
lhoda <lhodriscoll@wpi.edu>
2025-04-14 21:30:07 -0400

    Added title above the top left button

--- apps/frontend/src/service-request/ServiceRequestPage.tsx
+++ apps/frontend/src/service-request/ServiceRequestPage.tsx
@@ -6,52 +6,55 @@ export function ServiceRequestPage() {
     const navigate = useNavigate();
     return (
         <div>
-            <Box bg="themeGold.0">
-                <Title order={2} ta="center" c={'#001D4D'}>
-                    Select Request Type:
-                </Title>
+            <Box bg="themeGold.0" py="xl">
                 <Flex w="100%" h="85.5vh" justify="center" align="center">
-                    {/* basic grid for button layout*/}
-                    <SimpleGrid cols={3} spacing="50">
-                        {/* button for language interpreter request*/}
-                        <HoverButton
-                            icon={<IconLanguage size={120} />}
-                            label="Interpreter Request"
-                            onClick={() => navigate('/language-form')}
-                        />
-                        {/* button for ___ Request */}
-                        <HoverButton
-                            icon={<IconExclamationCircleFilled size={120} />}
-                            label="Type 2"
-                            onClick={() => navigate('/')}
-                        />
-                        {/* button for ___ Request */}
-                        <HoverButton
-                            icon={<IconExclamationCircleFilled size={120} />}
-                            label="Type 3"
-                            onClick={() => navigate('/')}
-                        />
-                        {/* button for ___ Request */}
-                        <HoverButton
-                            icon={<IconExclamationCircleFilled size={120} />}
-                            label="Type 4"
-                            onClick={() => navigate('/')}
-                        />
-                        {/* button for ___ Request */}
-                        <HoverButton
-                            icon={<IconExclamationCircleFilled size={120} />}
-                            label="Type 5"
-                            onClick={() => navigate('/')}
-                            disabled={true}
-                        />
-                        {/* button for ___ Request */}
-                        <HoverButton
-                            icon={<IconExclamationCircleFilled size={120} />}
-                            label="Type 6"
-                            onClick={() => navigate('/')}
-                            disabled={true}
-                        />
-                    </SimpleGrid>
+                    <Stack>
+                        <Title order={2} ta="left" c={'#001D4D'} mb="lg">
+                            Select Request Type:
+                        </Title>
+                        {/* basic grid for button layout*/}
+                        <SimpleGrid cols={3} spacing="50">
+                            {/* button for language interpreter request*/}
+                            <HoverButton
+                                icon={<IconLanguage size={120} />}
+                                label="Interpreter Request"
+                                onClick={() => navigate('/language-form')}
+                            />
+
+                            {/* button for ___ Request */}
+                            <HoverButton
+                                icon={<IconExclamationCircleFilled size={120} />}
+                                label="Type 2"
+                                onClick={() => navigate('/')}
+                            />
+                            {/* button for ___ Request */}
+                            <HoverButton
+                                icon={<IconExclamationCircleFilled size={120} />}
+                                label="Type 3"
+                                onClick={() => navigate('/')}
+                            />
+                            {/* button for ___ Request */}
+                            <HoverButton
+                                icon={<IconExclamationCircleFilled size={120} />}
+                                label="Type 4"
+                                onClick={() => navigate('/')}
+                            />
+                            {/* button for ___ Request */}
+                            <HoverButton
+                                icon={<IconExclamationCircleFilled size={120} />}
+                                label="Type 5"
+                                onClick={() => navigate('/')}
+                                disabled={true}
+                            />
+                            {/* button for ___ Request */}
+                            <HoverButton
+                                icon={<IconExclamationCircleFilled size={120} />}
+                                label="Type 6"
+                                onClick={() => navigate('/')}
+                                disabled={true}
+                            />
+                        </SimpleGrid>
+                    </Stack>
                 </Flex>
             </Box>
         </div>
