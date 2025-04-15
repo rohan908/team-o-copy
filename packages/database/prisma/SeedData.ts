efabcfdad70b5507a25bcd46c2f7d43e9be782a3
Owen Hart <owenvhart@gmail.com>
2025-04-15 14:48:32 -0400

    adds names for departments, fixes comments

--- packages/database/prisma/SeedData.ts
+++ packages/database/prisma/SeedData.ts
@@ -12,37 +12,37 @@ export function getNodeData() {
         { x: 12.8, y: 13.6, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [2,4]},
         { x: 10.3, y: 2.5, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [3,5]},
         { x: -0.9, y: 4.8, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [4,6]},
-        { x: -15.8, y: -6.3, floor: 1, name: 'Department 1', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [5,7]},
+        { x: -15.8, y: -6.3, floor: 1, name: 'Imaging Suite', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [5,7]},
         { x: -24.2, y: -4.6, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [6,8]},
         { x: -31.6, y: -35.9, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [7,9]},
         { x: -40.2, y: -34.4, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [8,10]},
         { x: -39.1, y: -30.0, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [9,11]},
-        { x: -36.6, y: -28.1, floor: 1, name: 'Department 2', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [10]},
+        { x: -36.6, y: -28.1, floor: 1, name: 'Pharmacy', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [10]},
 
-        // floor 1 Patriot-22
+        // floor 1 Patriot-22 (stairs to get to floors 3-4)
         { x: 115.2, y: 14.7, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [2,13]},
         { x: 86.0, y: 2.9, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [12,14]},
         { x: 92.5, y: -15.8, floor: 1, name: 'staircase-1', description: 'stairs', nodeType: 'staircase', mapId: 1, connectingNodes: [13,15]},
 
-        // floor 2 Patriot-22
+        // floor 3 Patriot-22 (listed as 2 for pathfinding)
         { x: 94.8, y: -15.5, floor: 2, name: 'staircase-2', description: 'stairs', nodeType: 'staircase', mapId: 1, connectingNodes: [14,16,25]},
         { x: 87.2, y: -18.8, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [15,17,19]},
         { x: 84.9, y: -20.9, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [16,18]},
-        { x: 80.9, y: -18.8, floor: 2, name: 'Department 3', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [17]},
+        { x: 80.9, y: -18.8, floor: 2, name: 'Vein Care Services', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [17]},
         { x: 88.4, y: -24.2, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [16,20]},
         { x: 72.3, y: -28.9, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [19,21]},
         { x: 69.6, y: -23.4, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [20,22]},
         { x: 24.4, y: -39.7, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [21,23]},
         { x: 30.3, y: -58.4, floor: 2, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [22,24]},
-        { x: 24.7, y: -60.1, floor: 2, name: 'Department 4', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [23]},
+        { x: 24.7, y: -60.1, floor: 2, name: 'Endocrinology', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [23]},
 
-        // floor 3 Patriot-22
+        // floor 4 Patriot-22 (listed as 3 for pathfinding)
         { x: 94.6, y: -15.3, floor: 3, name: 'staircase-3', description: 'stairs', nodeType: 'staircase', mapId: 1, connectingNodes: [15,26]},
         { x: 81.1, y: -20.6, floor: 3, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [25,27]},
         { x: 75.6, y: -20.3, floor: 3, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [26,28]},
         { x: 52.4, y: -28.5, floor: 3, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [27,29]},
         { x: 50.6, y: -23.8, floor: 3, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 1, connectingNodes: [28,30]},
-        { x: 54.8, y: -22.0, floor: 3, name: 'Department 5', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [29]},
+        { x: 54.8, y: -22.0, floor: 3, name: 'Phlebotomy/Primary Care', description: 'department', nodeType: 'department', mapId: 1, connectingNodes: [29]},
 
         // starting at node ID 100 -->
         // floor 1 Chestnut hill
@@ -58,11 +58,11 @@ export function getNodeData() {
         { id: 109, x: 87.1, y: 1.4, floor: 1, name: 'CH Parking Lot 3', description: 'parking', nodeType: 'parking-lot', mapId: 2, connectingNodes: [108,110]},
         { id: 110, x: 84.3, y: -24.0, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 2, connectingNodes: [109,111]},
         { id: 111, x: 70.9, y: -24.1, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 2, connectingNodes: [110,112,116]},
-        { id: 112, x: 70.6, y: -12.3, floor: 1, name: 'Department 1', description: 'department', nodeType: 'department', mapId: 2, connectingNodes: [111]},
+        { id: 112, x: 70.6, y: -12.3, floor: 1, name: 'Multi-Specialty Clinic', description: 'department', nodeType: 'department', mapId: 2, connectingNodes: [111]},
         { id: 113, x: 34.2, y: -12.5, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 2, connectingNodes: [104,114]},
         { id: 114, x: 31.4, y: -18.6, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 2, connectingNodes: [113,115]},
         { id: 115, x: 33.0, y: -25.0, floor: 1, name: 'hallway', description: 'hallway', nodeType: 'hallway', mapId: 2, connectingNodes: [114,116]},
-        { id: 116, x: 57.4, y: -25.3, floor: 1, name: 'Department 2', description: 'department', nodeType: 'department', mapId: 2, connectingNodes: [115,111]},
+        { id: 116, x: 57.4, y: -25.3, floor: 1, name: 'Radiology', description: 'department', nodeType: 'department', mapId: 2, connectingNodes: [115,111]},
 
 
 
