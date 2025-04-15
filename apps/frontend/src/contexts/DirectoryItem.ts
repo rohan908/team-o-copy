6a4af44ab641fbb5463c664dc0d6a7c83178dcf9
Exhan-r <ethanramoth63@gmail.com>
2025-04-14 21:40:13 -0400

    Fixed a mistake I made

---
+++ apps/frontend/src/contexts/DirectoryItem.ts
@@ -0,0 +1,7 @@
+// defines the fields of a directory item (i.e. Patriot20, Patriot22)
+export interface DirectoryItem {
+  title: string;
+  slug: string;
+  description: string;
+  coords: number[];
+}
