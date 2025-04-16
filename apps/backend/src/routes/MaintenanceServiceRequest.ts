64978ab626932b9934114b06bc26f158a3d579d8
LoganWin <logan9win@gmail.com>
2025-04-16 02:29:36 -0400

    final changes

--- apps/backend/src/routes/MaintenanceServiceRequest.ts
+++ apps/backend/src/routes/MaintenanceServiceRequest.ts
@@ -1,5 +1,6 @@
 import express, { Router, Request, Response } from 'express';
 import PrismaClient from '../bin/prisma-client';
+import { convertTo24Hour } from './helperFunctions.ts';
 
 const router: Router = express.Router();
 
@@ -52,17 +53,5 @@ router.get('/', async (req: Request, res: Response) => {
     const allServiceRequests = await PrismaClient.maintenanceServiceRequest.findMany({});
     res.json(allServiceRequests);
 });
-
-// helper function for converting the database time value
-// to a more readable string
-function convertTo24Hour(time: string): string {
-    const [raw, modifier] = time.split(' ');
-    let [hours, minutes] = raw.split(':').map(Number);
-
-    if (modifier === 'PM' && hours !== 12) hours += 12;
-    if (modifier === 'AM' && hours === 12) hours = 0;
-
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-}
 
 export default router;
