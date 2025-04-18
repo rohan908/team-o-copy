f3ac740936792d72530e1861b21341466aaba815
Owen Hart <owenvhart@gmail.com>
2025-04-18 17:43:30 -0400

    adds hover effect to request buttons, perspective still needs work

--- apps/frontend/src/service-request/components/HoverButton.tsx
+++ apps/frontend/src/service-request/components/HoverButton.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Button, Stack } from '@mantine/core';
+import React, { useState, useCallback } from 'react';
+import { Button, Stack, Transition } from '@mantine/core';
+import { useHover, useMouse, useMergedRef } from '@mantine/hooks';
 
 interface HoverButtonProps {
   icon: React.ReactNode;
@@ -9,27 +10,58 @@ interface HoverButtonProps {
 }
 // exporting to own file to utilize useState to change color on hover
 const HoverButton: React.FC<HoverButtonProps> = ({ icon, label, onClick, disabled = false }) => {
-  const [hovered, setHovered] = useState(false);
+  const { hovered, ref: hoverRef } = useHover();
+  const { x, y, ref: mouseRef } = useMouse({ resetOnExit: true});
+
+  const mergedRef = useMergedRef(
+    hoverRef,
+    mouseRef
+  );
+
+  const xRotation = useCallback((buttonHeight: number) => {
+    return -(y - buttonHeight/2);
+  }, [y])
+
+  const yRotation = useCallback((buttonWidth: number) => {
+    return (x - buttonWidth/2);
+  }, [x])
 
   return (
-    <Button
-      disabled={disabled}
-      onClick={onClick}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      style={{
-        width: 300,
-        height: 250,
-        padding: '1.5rem',
-        backgroundColor: hovered ? '#002B70' : '#003A97',
-      }}
-    >
-      <Stack align="center">
-        {icon}
-        <span>{label}</span>
-      </Stack>
-    </Button>
+      <Button
+        ref={mergedRef}
+        disabled={disabled}
+        onClick={onClick}
+        style={{
+          width: 300,
+          height: 250,
+          padding: '1.5rem',
+          backgroundColor: hovered ? '#002B70' : '#003A97',
+          transform: hovered ? `rotateX(${xRotation(250)/3}deg) rotateY(${yRotation(300)/3}deg) scale(1.1)` : 'scale(1)',
+          transition: "all 0.25s linear",
+          perspective: "100px",
+          boxShadow: '10px 10px 20px rgba(36, 36, 36, 0.5)',
+        }}
+      >
+        <Stack align="center">
+          {icon}
+          <span>{label}</span>
+        </Stack>
+      </Button>
   );
 };
 
 export default HoverButton;
+
+/*
+<Transition
+  mounted={hovered}
+  transition="scale"
+  duration={500}
+  timingFunction="ease"
+>
+  {transitionStyle => (
+
+  )}
+</Transition>
+
+ */
