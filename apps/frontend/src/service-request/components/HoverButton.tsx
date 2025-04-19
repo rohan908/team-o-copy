d6ef3e4466e2ba95577a99f23e77cc355996b5fe
Owen Hart <owenvhart@gmail.com>
2025-04-19 10:34:09 -0400

    full 3d hover buttons for service requests, needs to be component based

--- apps/frontend/src/service-request/components/HoverButton.tsx
+++ apps/frontend/src/service-request/components/HoverButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { Button, Stack, Transition } from '@mantine/core';
+import { Button, Stack, Transition, Overlay } from '@mantine/core';
 import { useHover, useMouse, useMergedRef } from '@mantine/hooks';
 
 interface HoverButtonProps {
@@ -36,32 +36,29 @@ const HoverButton: React.FC<HoverButtonProps> = ({ icon, label, onClick, disable
           height: 250,
           padding: '1.5rem',
           backgroundColor: hovered ? '#002B70' : '#003A97',
-          transform: hovered ? `rotateX(${xRotation(250)/3}deg) rotateY(${yRotation(300)/3}deg) scale(1.1)` : 'scale(1)',
+          border: '10px solid #003A97',
+          transform: hovered ? `rotateX(${xRotation(250)/5}deg) rotateY(${yRotation(300)/5}deg) scale(1.1)` : 'scale(1)',
           transition: "all 0.25s linear",
           perspective: "100px",
-          boxShadow: '10px 10px 20px rgba(36, 36, 36, 0.5)',
+          boxShadow: hovered ? '10px 10px 20px 10px rgba(36, 36, 36, 0.5)' : '10px 10px 20px rgba(36, 36, 36, 0.5)',
+
         }}
       >
-        <Stack align="center">
-          {icon}
-          <span>{label}</span>
-        </Stack>
+        <Overlay style={{
+          padding: '1.5rem',
+          backgroundColor: hovered ? '#002B70' : '#003A97',
+          transition: "all 0.25s linear",}}>
+          <Stack align="center"
+                 style={{
+                   transform: hovered ? `translate3d(${yRotation(250)/5}px, ${-xRotation(300)/5}px, 20px) scale(0.9)` : `translateZ(0px)`,
+                   transition: "all 0.25s linear",
+                 }}>
+            {icon}
+            <span>{label}</span>
+          </Stack>
+        </Overlay>
       </Button>
   );
 };
 
 export default HoverButton;
-
-/*
-<Transition
-  mounted={hovered}
-  transition="scale"
-  duration={500}
-  timingFunction="ease"
->
-  {transitionStyle => (
-
-  )}
-</Transition>
-
- */
