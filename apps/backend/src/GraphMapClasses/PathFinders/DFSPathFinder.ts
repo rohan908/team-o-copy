b1290c02b8b1dab80724277b2c11747f69846d71
hudsonkortus <hkortus@wpi.edu>
2025-04-20 11:41:19 -0400

    half way thorugh bfs recursion

--- apps/backend/src/GraphMapClasses/PathFinders/DFSPathFinder.ts
+++ apps/backend/src/GraphMapClasses/PathFinders/DFSPathFinder.ts
@@ -8,5 +8,25 @@ export class DFSPathFinder extends PathFinder {
   constructor(graphRef: () => Graph<NodeDataType>) {
     super(graphRef);
   }
+   findPath(startNodeId: number, endNodeId: number): PathFinderResult {
+     const graph: Graph<NodeDataType> = this.graphRef(); // we store locally at the start of the funciton since the graph won't change while doing the path planning operations
+     const startNode = graph.getNodeById(startNodeId); //note: do not define type bc graph and nodes use generics
+     const endNode = graph.getNodeById(endNodeId);
 
+     if (!startNode || !endNode) {
+       console.error('start or end node DNE');
+       return { success: false, pathIDs: [], distance: 0 };
+     }
+
+
+   }
+
+   //check if
+   bfs(graph: Graph<NodeDataType>, target: Graph<NodeDataType>, current: Graph<NodeDataType> ) {
+      if (target == current){
+        succsess = true
+      } else {
+
+      }
+   }
 }
