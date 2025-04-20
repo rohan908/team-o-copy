556e55417c01f3f1d018a3df4c56a993991b98a8
Owen Hart <owenvhart@gmail.com>
2025-04-20 13:24:21 -0400

    changes color slightly, merges with main, adds comments

--- apps/frontend/src/service-request/components/HoverButton.tsx
+++ apps/frontend/src/service-request/components/HoverButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { Button, Stack, Transition, Overlay } from '@mantine/core';
+import { Button, Stack, Transition, Overlay, useMantineTheme } from '@mantine/core';
 import { useHover, useMouse, useMergedRef } from '@mantine/hooks';
 
 interface HoverButtonProps {
@@ -8,10 +8,15 @@ interface HoverButtonProps {
   onClick?: () => void;
   disabled?: boolean;
 }
-// exporting to own file to utilize useState to change color on hover
+
+// component for large service request buttons
 const HoverButton: React.FC<HoverButtonProps> = ({ icon, label, onClick, disabled = false }) => {
+  const theme = useMantineTheme();
   const { hovered, ref: hoverRef } = useHover();
   const { x, y, ref: mouseRef } = useMouse({ resetOnExit: true});
+
+  // limits the button rotation, lower = bigger effect
+  const rotationConstrain: number = 5;
 
   const mergedRef = useMergedRef(
     hoverRef,
@@ -19,38 +24,39 @@ const HoverButton: React.FC<HoverButtonProps> = ({ icon, label, onClick, disable
   );
 
   const xRotation = useCallback((buttonHeight: number) => {
-    return -(y - buttonHeight/2);
+    return -(y - buttonHeight/2)/rotationConstrain;
   }, [y])
 
   const yRotation = useCallback((buttonWidth: number) => {
-    return (x - buttonWidth/2);
+    return (x - buttonWidth/2)/rotationConstrain;
   }, [x])
 
+  // in-line styling is needed for the CSS hover effects
+  // one hard-coded color for meeting the style guide
   return (
       <Button
         ref={mergedRef}
         disabled={disabled}
         onClick={onClick}
         style={{
-          width: 300,
-          height: 250,
+          width: '300px',
+          height: '250px',
           padding: '1.5rem',
-          backgroundColor: hovered ? '#002B70' : '#003A97',
-          border: '10px solid #003A97',
-          transform: hovered ? `rotateX(${xRotation(250)/5}deg) rotateY(${yRotation(300)/5}deg) scale(1.1)` : 'scale(1)',
+          border: '10px solid' + theme.colors.blueBase[8],
+          transform: hovered ? `rotateX(${xRotation(250)}deg) rotateY(${yRotation(300)}deg) scale(1.1)` : 'scale(1)',
           transition: "all 0.25s linear",
           perspective: "100px",
-          boxShadow: hovered ? '10px 10px 20px 10px rgba(36, 36, 36, 0.5)' : '10px 10px 20px rgba(36, 36, 36, 0.5)',
-
+          boxShadow: hovered ? '10px 10px 20px 5px rgba(36, 36, 36, 0.5)' : '00px 00px 0px',
         }}
       >
         <Overlay style={{
           padding: '1.5rem',
-          backgroundColor: hovered ? '#002B70' : '#003A97',
+          // the one hard-coded color from style guide, not in mantine yet
+          backgroundColor: hovered ? '#003EB1' : theme.colors.blueBase[8],
           transition: "all 0.25s linear",}}>
           <Stack align="center"
                  style={{
-                   transform: hovered ? `translate3d(${yRotation(250)/5}px, ${-xRotation(300)/5}px, 20px) scale(0.9)` : `translateZ(0px)`,
+                   transform: hovered ? `translate3d(${yRotation(250)}px, ${-xRotation(300)}px, 20px) scale(0.9)` : `translateZ(0px)`,
                    transition: "all 0.25s linear",
                  }}>
             {icon}
