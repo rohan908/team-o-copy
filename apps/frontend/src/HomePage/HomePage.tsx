c9bcbc32eb409497f3af840da4de4caffb27884e
rohan908 <145878828+rohan908@users.noreply.github.com>
2025-04-22 11:52:04 -0400

    HOVER COMPNENT WORKS LETS GOOOOO

--- apps/frontend/src/HomePage/HomePage.tsx
+++ apps/frontend/src/HomePage/HomePage.tsx
@@ -1,23 +1,27 @@
-import {Box, Flex, Grid, Stack, Title, useMantineTheme} from '@mantine/core';
+import { Box, Flex, Grid, Stack, Title, useMantineTheme } from '@mantine/core';
 import { CustomTimeline } from './CustomTimeline.tsx';
 import { useState } from 'react';
 import { ContentSwitcher } from './ContentSwitcher.tsx';
-import { TimelineProvider } from './TimeLineContext.tsx';
 import { HoverUnderline } from '../common-compoents/HoverUnderline.js';
 
 export function HomePage() {
     const theme = useMantineTheme();
 
     return (
-          <Flex h="100%" justify="center" align="center" bg={theme.colors.terquAccet[1]}> {/* background */}
-            <Flex justify={'space-around'} wrap={'wrap'} > {/*row flex of info inputs and picture side by side*/}
-                <Stack h={"90%"} pt={"xl"}>
-                  <HoverUnderline>
-                    <Title order={1}>How can We Help?</Title> {/* h1 heading */}
-                  </HoverUnderline>
+        <Flex h="100%" justify="center" align="center" bg={theme.colors.primaryBlues[1]}>
+            {' '}
+            {/* background */}
+            <Flex justify={'space-around'} wrap={'wrap'}>
+                {' '}
+                {/*row flex of info inputs and picture side by side*/}
+                <Stack h={'90%'} pt={'xl'}>
+                    <HoverUnderline>
+                        <Title order={1}>How can We Help?</Title> {/* h1 heading */}
+                    </HoverUnderline>
+                    <CustomTimeline />
                 </Stack>
-                <Image src
+                <ContentSwitcher />
             </Flex>
-          </Flex>
+        </Flex>
     );
 }
--- apps/frontend/src/common-compoents/HoverUnderline.tsx
+++ apps/frontend/src/common-compoents/HoverUnderline.tsx
@@ -1,37 +1,12 @@
-import { Box, BoxProps, createStyles, MantineTheme, useMantineTheme } from '@mantine/core';
-
-
 // effect from: https://stackoverflow.com/questions/70906977/tailwind-underline-hover-animation
-const useStyles = createStyles((theme : MantineTheme) => ({
-  container: {
-    position: 'relative',
-    display: 'inline-block',
-    cursor: 'pointer',
-  },
-  underline: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    height: '2px',
-    width: '80%',
-    backgroundColor: theme.colors.yellowAccent[4],
-    transition: 'width 300ms ease',
-  },
-  hoverEffect: {
-    '&:hover .underline': {
-      width: '100%',
-    },
-  },
-}));
-
-export function HoverUnderline(props: BoxProps) {
-  const theme = useMantineTheme();
-  const { classes } = useStyles(theme);
-
-  return (
-    <Box className={`${classes.container} ${classes.hoverEffect}`} {...props}>
-      {props.children}
-      <div className="underline" />
-    </Box>
-  );
+import { Box, BoxProps } from '@mantine/core';
+import classes from './HoverUnderline.module.css';
+import { PropsWithChildren } from 'react';
+export function HoverUnderline({children, ...props}: PropsWithChildren<BoxProps>) {
+    return (
+        <Box className={classes.container} {...props}>
+            {children}
+            <div className={classes.underline} />
+        </Box>
+    );
 }
