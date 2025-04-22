de07f23255094c58a4de5f41d3e334ae8c12e16e
Exhan-r <ethanramoth63@gmail.com>
2025-04-22 18:35:08 -0400

    Made the new nav bar, still has some glaring issues

--- apps/frontend/src/home-page/components/nav-bar.tsx
+++ apps/frontend/src/home-page/components/nav-bar.tsx
@@ -21,109 +21,117 @@ import { MapEditor } from '../../IndoorMapPage/MapEditor.tsx';
 import { useDisclosure } from '@mantine/hooks';
 
 type NavItem = {
-    name: string;
-    link: string;
+  name: string;
+  link: string;
 };
 
 export const navItems: NavItem[] = [
-    { name: 'Navigation', link: '/map-API' },
+  { name: 'Navigation', link: '/map-API' },
 ];
 
 export const adminNavItems: NavItem[] = [
-    { name: 'Service Request', link: '/service-request-page' }, //add service rec routting here logan
-    { name: 'Admin Page', link: '/admin-page' },
-    { name: 'Map Editor', link: '/map-editor' },
+  { name: 'Service Request', link: '/service-request-page' }, //add service rec routing here logan
+  { name: 'Admin Page', link: '/admin-page' },
+  { name: 'Map Editor', link: '/map-editor' },
 
-    // { name: "Profile", link: "/submission" }// potential delighter- login button can be in this
+  // { name: "Profile", link: "/submission" }// potential delighter- login button can be in this
 ];
 
 export function NavBar() {
-    const { isLoggedIn, logout } = useLogin();
-    return (
-        <>
-            <nav>
-                <Group h="100%" px="md" py="sm" bg={"transparent"}>
-                    <Group justify="space-between" style={{ flex: 1 }}>
-                        {/* Logo */}
-                        <Link to="/">
-                          <Flex
-                            bg="blueBase.8"
-                            w="50px"
-                            h="50px"
-                            style={{
-                              borderRadius: "50%",
-                            }}
-                            justify={"center"}
-                            align="center"
-                          >
-                            <img
-                              height="25px"
-                              width="25px"
-                              src={"/goldLogoMassGeneralBrigham.png"}>
-                            </img>
-                          </Flex>
-                        </Link>
+  const { isLoggedIn, logout } = useLogin();
+  return (
+    <>
+      <Box
+        component="nav"
+        pos="sticky"
+        top={0}
+        bg="transparent"
+        style={{ zIndex: 999 }}
+      >
+        <Group h="100%" px="md" py="sm" bg={"transparent"}>
+          <Group justify="space-between" style={{ flex: 1 }}>
+            {/* Logo */}
+            <Link to="/">
+              <Flex
+                bg="blueBase.8"
+                w="50px"
+                h="50px"
+                style={{
+                  borderRadius: "50%",
+                }}
+                justify={"center"}
+                align="center"
+              >
+                <img
+                  height="25px"
+                  width="25px"
+                  src={"/goldLogoMassGeneralBrigham.png"}>
+                </img>
+              </Flex>
+            </Link>
 
-                        <Group ml="xl" gap="md" visibleFrom="sm" bg="blueBase.9" style={{
-                          borderRadius: "20px",
-                        }}>
-                          <Box m="3px" bg="blueBase.6" style={{
-                            borderRadius: "20px",
-                          }}>
-                            <Box m="3px" bg="blueBase.9" style={{
-                              borderRadius: "20px",
-                            }}>
-                            {/* Dynamically Render Buttons */}
-                            {navItems.map((item, index) => (
-                                <Button
-                                    variant="filled"
-                                    color="baseBlue.6"
-                                    className="navButton"
-                                    justify="flex-end"
-                                    component={Link}
-                                    to={item.link}
-                                    size="sm"
-                                >
-                                    {item.name}
-                                </Button>
-                            ))}
-                            {isLoggedIn && (
-                                <>
-                                    {adminNavItems.map((item, index) => (
-                                        <Button
-                                            variant="filled"
-                                            color="baseBlue.6"
-                                            className="navButton"
-                                            justify="flex-end"
-                                            component={Link}
-                                            to={item.link}
-                                            size="sm"
-                                        >
-                                            {item.name}
-                                        </Button>
-                                    ))}
-                                    {/* Logout Button */}
-                                    <Button
-                                        variant="filled"
-                                        color="baseBlue.6"
-                                        className="LoggoutButton"
-                                        justify="flex-end"
-                                        onClick={logout}
-                                        component={Link}
-                                        to={'/'}
-                                        size="sm"
-                                    >
-                                        Log Out
-                                    </Button>
-                                </>
-                            )}
-                            </Box>
-                          </Box>
-                        </Group>
-                    </Group>
-                </Group>
-            </nav>
-            <Outlet />
-        </>
-    );
+            <Group ml="xl" gap="md" visibleFrom="sm" bg="blueBase.9" style={{
+              borderRadius: "20px",
+            }}>
+              <Box m="3px" bg="blueBase.6" style={{
+                borderRadius: "20px",
+              }}>
+                <Box m="3px" bg="blueBase.9" style={{
+                  borderRadius: "20px",
+                }}>
+                  {/* Dynamically Render Buttons */}
+                  {navItems.map((item, index) => (
+                    <Button
+                      variant="filled"
+                      color="baseBlue.6"
+                      className="navButton"
+                      justify="flex-end"
+                      component={Link}
+                      to={item.link}
+                      size="sm"
+                    >
+                      {item.name}
+                    </Button>
+                  ))}
+                  {isLoggedIn && (
+                    <>
+                      {adminNavItems.map((item, index) => (
+                        <Button
+                          variant="filled"
+                          color="baseBlue.6"
+                          className="navButton"
+                          justify="flex-end"
+                          component={Link}
+                          to={item.link}
+                          size="sm"
+                        >
+                          {item.name}
+                        </Button>
+                      ))}
+                      {/* Logout Button */}
+                      <Button
+                        variant="filled"
+                        color="baseBlue.6"
+                        className="LoggoutButton"
+                        justify="flex-end"
+                        onClick={logout}
+                        component={Link}
+                        to={'/'}
+                        size="sm"
+                      >
+                        Log Out
+                      </Button>
+                    </>
+                  )}
+                </Box>
+              </Box>
+            </Group>
+          </Group>
+        </Group>
+      </Box>
+      <Box style={{ paddingTop: '70px' }}>
+        <Outlet />
+      </Box>
+    </>
+  );
 }
