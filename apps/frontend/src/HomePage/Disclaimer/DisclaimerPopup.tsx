ad9a73b4447404ae505305b9e34d66783bc20517
jabata29 <jabata756@gmail.com>
2025-04-22 23:08:43 -0400

    final disclaimer popup

--- apps/frontend/src/HomePage/Disclaimer/DisclaimerPopup.tsx
+++ apps/frontend/src/HomePage/Disclaimer/DisclaimerPopup.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Card, Image, Text, Badge, Button, Group, Transition } from '@mantine/core';
+import { useState, useEffect } from 'react';
+import { Card, Image, Text, Badge, Box, Container, Group, Portal, Transition } from '@mantine/core';
 
 /*
   yo this code is 🔥🔥
@@ -9,36 +9,56 @@ import { Card, Image, Text, Badge, Button, Group, Transition } from '@mantine/co
 export function DisclaimerPopup() {
     const [visible, setVisible] = useState(true);
 
+    useEffect(() => {
+        const timer = setTimeout(() => setVisible(false), 3500);
+        /*
+         this is for deleting the timer after the page dismounts
+         */
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <Transition
-            mounted={visible}
-            transition="fade"
-            duration={15000}
-            exitDelay={15000}
-            onExited={() => setVisible(false)}
-        >
-            {(transitionStyle) => (
-                <Card shadow="sm" padding="lg" radius="md" withBorder style={transitionStyle}>
-                    <Card.Section>
-                        <Image
-                            src="../../../public/DisclaimerImages/waitWaitWait.gif"
-                            height={160}
-                            alt="Brigham Women's Hospital Picture!!!"
-                        />
-                    </Card.Section>
+        <Portal>
+            <Transition
+                mounted={visible}
+                transition="fade"
+                duration={500}
+                exitDelay={1}
+                timingFunction="ease"
+            >
+                {(transitionStyle) => (
+                    <Box pos="fixed" top={20} left="50%" style={{ transform: 'translateX(-50%)' }}>
+                        <Container size="xs" px="md">
+                            <Card
+                                shadow="sm"
+                                padding="lg"
+                                radius="md"
+                                withBorder
+                                style={transitionStyle}
+                            >
+                                <Card.Section>
+                                    <Image
+                                        src="../../../public/DisclaimerImages/waitWaitWait.gif"
+                                        height={160}
+                                        alt="Brigham Women's Hospital Picture!!!"
+                                    />
+                                </Card.Section>
 
-                    <Group justify="space-between" mt="md" mb="xs">
-                        <Text fw={500}>Disclaimer:</Text>
-                        <Badge color="F8D261">PLEASE READ</Badge>
-                    </Group>
+                                <Group justify="space-between" mt="md" mb="xs">
+                                    <Text fw={500}>Disclaimer</Text>
+                                    <Badge color="yellow">IMPORTANT</Badge>
+                                </Group>
 
-                    <Text size="sm" c="dimmed">
-                        This web application is strictly a CS3733-D25 Software Engineering class
-                        project for Prof. Wilson Wong at WPI
-                    </Text>
-                </Card>
-            )}
-        </Transition>
+                                <Text size="sm" c="dimmed">
+                                    This web application is strictly a CS3733-D25 Software
+                                    Engineering class project for Prof. Wilson Wong at WPI
+                                </Text>
+                            </Card>
+                        </Container>
+                    </Box>
+                )}
+            </Transition>
+        </Portal>
     );
 }
 
