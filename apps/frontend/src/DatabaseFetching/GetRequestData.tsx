1bf2f35b6ea4eed90eb0033ebbf1301c233e8d4b
lhoda <lhodriscoll@wpi.edu>
2025-04-23 17:55:19 -0400

    fetch fix

--- apps/frontend/src/DatabaseFetching/GetRequestData.tsx
+++ apps/frontend/src/DatabaseFetching/GetRequestData.tsx
@@ -3,7 +3,7 @@ export async function fetchRequestData(requestName: string) {
         console.log('fetching');
 
         // gets directory data through http query from directory.ts
-        const res = await fetch('/api/${requestName}');
+        const res = await fetch(`/api/${requestName}`);
 
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
@@ -11,7 +11,6 @@ export async function fetchRequestData(requestName: string) {
 
         // returns data as a Promise !!
         return res;
-
     } catch (error) {
         console.error('Error fetching request table', error);
         return null;
