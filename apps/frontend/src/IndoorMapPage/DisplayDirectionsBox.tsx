707e6f20e2f9f89a1b59fcc043f996a2f6a1e919
hudsonkortus <hkortus@wpi.edu>
2025-04-29 22:56:26 -0400

    it works now

--- apps/frontend/src/IndoorMapPage/DisplayDirectionsBox.tsx
+++ apps/frontend/src/IndoorMapPage/DisplayDirectionsBox.tsx
@@ -27,177 +27,209 @@ import TTSButton from '../Buttons/TTSButton.tsx';
  *
  */
 function getPathNodes(nodeIds: number[], allNodes: NodeDataType[]): NodeDataType[] {
+    const pathNodes: NodeDataType[] = [];
 
-  const pathNodes: NodeDataType[] = [];
-
-  for (const id of nodeIds) {
-    const node = allNodes.find((node) => node.id === id);
-    if (node) {
-      pathNodes.push(node);
+    for (const id of nodeIds) {
+        const node = allNodes.find((node) => node.id === id);
+        if (node) {
+            pathNodes.push(node);
+        }
     }
-  }
 
-  return pathNodes;
+    return pathNodes;
 }
 
 export function DisplayDirectionsBox() {
-  const {
-    department,
-    selectedAlgorithm,
-    selectedHospital,
-  } = useTimeline();
-  const pathNodes = usePathContext();
-  const navSelection = useNavSelectionContext();
-  const nodeIds = pathNodes.state.pathSelectRequest?.NodeIds;
+    const { department, selectedHospital } = useTimeline();
+    const pathNodes = usePathContext();
+    const navSelection = useNavSelectionContext();
+    const nodeIds = pathNodes.state.pathSelectRequest?.NodeIds;
 
-  const allNodes = useAllNodesContext();
+    const allNodes = useAllNodesContext();
 
-  // Scale factor translates node space coords to feet
-  const getScaleFactor = () => {
-    const hospital = navSelection.state.navSelectRequest?.HospitalName;
-    if (hospital === '20 Patriot Pl' || hospital === '22 Patriot Pl') {
-      return 1.5;
-    }
-    if (hospital === 'Chestnut Hill') {
-      return 0.91;
-    }
-    if (hospital === 'Faulkner Hospital') {
-      return 2.23;
-    }
-    if (hospital === 'BWH Campus') {
-      return 3.43;
-    } else {
-      return 1;
-    }
-  };
+    // Scale factor translates node space coords to feet
+    const getScaleFactor = () => {
+        const hospital = navSelection.state.navSelectRequest?.HospitalName;
+        if (hospital === '20 Patriot Pl' || hospital === '22 Patriot Pl') {
+            return 1.5;
+        }
+        if (hospital === 'Chestnut Hill') {
+            return 0.91;
+        }
+        if (hospital === 'Faulkner Hospital') {
+            return 2.23;
+        }
+        if (hospital === 'BWH Campus') {
+            return 3.43;
+        } else {
+            return 1;
+        }
+    };
 
-  // only call GetTextDirections if/when pathNodes change
-  const directions = useMemo(() => {
-    if (nodeIds && nodeIds.length > 1) {
-      const fullPathNodes = getPathNodes(nodeIds, allNodes);
-      return GetTextDirections(fullPathNodes);
-    }
-    return [];
-  }, [nodeIds, allNodes]);
-  // group directions by floor so they can be displayed separately
+    // only call GetTextDirections if/when pathNodes change
+    const directions = useMemo(() => {
+        if (nodeIds && nodeIds.length > 1) {
+            const fullPathNodes = getPathNodes(nodeIds, allNodes);
+            return GetTextDirections(fullPathNodes);
+        }
+        return [];
+    }, [nodeIds, allNodes]);
+    // group directions by floor so they can be displayed separately
 
-  const directionsByFloor: {
-    [floor: number]: { Direction: string; Distance: number; Floor: number }[];
-  } = {};
-  directions.forEach((direction) => {
-    if (!directionsByFloor[direction.Floor]) {
-      directionsByFloor[direction.Floor] = [];
+    const directionsByFloor: {
+        [floor: number]: { Direction: string; Distance: number; Floor: number }[];
+    } = {};
+    directions.forEach((direction) => {
+        if (!directionsByFloor[direction.Floor]) {
+            directionsByFloor[direction.Floor] = [];
+        }
+        directionsByFloor[direction.Floor].push(direction);
+    });
+    if (!department || !selectedHospital) {
+        return (
+            <Box
+                w="80%"
+                h="400px"
+                style={{
+                    overflow: 'hidden',
+                    borderRadius: '8px',
+                    boxShadow:
+                        nodeIds && nodeIds.length > 1 && department && selectedHospital
+                            ? '0px 0px 4px 0px #AAAAAA'
+                            : '0px 0px 0px 0px #FFFFFF',
+                }}
+            >
+                <Box style={{ overflowY: 'auto' }}>
+                    <Accordion
+                        multiple
+                        styles={{
+                            root: {
+                                height: '450px',
+                                overflowY: 'auto',
+                            },
+                            item: {
+                                marginBottom: '0px',
+                            },
+                        }}
+                        defaultValue={[
+                            'floor-1',
+                            'floor-2',
+                            'floor-3',
+                            'floor-4',
+                            'floor-5',
+                            'floor-6',
+                            'floor-7',
+                        ]}
+                    ></Accordion>
+                </Box>
+            </Box>
+        );
     }
-    directionsByFloor[direction.Floor].push(direction);
-  });
-  if (!department || !selectedAlgorithm || !selectedHospital) {
     return (
-      <Box w="80%" h="400px" style={{overflow: 'hidden', borderRadius: "8px", boxShadow: nodeIds && nodeIds.length > 1 && department && selectedAlgorithm && selectedHospital ? "0px 0px 4px 0px #AAAAAA" : "0px 0px 0px 0px #FFFFFF" }}>
         <Box
-          style={{overflowY: 'auto'}}
-        >
-          <Accordion
-            multiple
-            styles={{
-              root: {
-                height: "450px",
-                overflowY: "auto",
-              },
-              item: {
-                marginBottom: '0px',
-              },
+            w="80%"
+            h="400px"
+            style={{
+                overflow: 'hidden',
+                borderRadius: '8px',
+                boxShadow:
+                    nodeIds && nodeIds.length > 1 && department && selectedHospital
+                        ? '0px 0px 4px 0px #AAAAAA'
+                        : '0px 0px 0px 0px #FFFFFF',
             }}
-            defaultValue={["floor-1", "floor-2", "floor-3", "floor-4", "floor-5","floor-6", "floor-7"]}
-          >
-          </Accordion>
-        </Box>
-      </Box>
-    )
-  }
-  return (
-    <Box w="80%" h="400px" style={{overflow: 'hidden', borderRadius: "8px", boxShadow: nodeIds && nodeIds.length > 1 && department && selectedAlgorithm && selectedHospital ? "0px 0px 4px 0px #AAAAAA" : "0px 0px 0px 0px #FFFFFF" }}>
-      <Box
-        style={{overflowY: 'auto'}}
-      >
-        <Accordion
-          multiple
-          styles={{
-            root: {
-              height: "450px",
-              overflowY: "auto",
-            },
-            item: {
-              marginBottom: '0px',
-            },
-          }}
-          defaultValue={["floor-1", "floor-2", "floor-3", "floor-4", "floor-5","floor-6", "floor-7"]}
         >
-          {Object.entries(directionsByFloor).map(([floor, direction]) => {
-            const floorTTS = direction.map((step) => {
-              if (step.Direction.startsWith('Take')) return step.Direction;
-              if (step.Direction === 'Straight')
-                return `Continue straight for ${(step.Distance * getScaleFactor()).toFixed(0)} feet.`;
-              return `Turn ${step.Direction.toLowerCase()}`;
-            });
-            return (
-              <Accordion.Item key={floor} value={`floor-${floor}`}>
-                <Accordion.Control>
-                  <Group>
-                    <Text fw={700} size="md" c="blue.7">
-                      {/* Stupid ass logic to change the floor bc we didn't do it right the first time*/}
-                      {/* Changing for Faulkner and Chestnut*/}
-                      {Number(floor) === 1 ? 'Floor 1' : ""}
-                      {Number(floor) === 2 ? 'Floor 3' : ""}
-                      {Number(floor) === 3 ? 'Floor 4' : ""}
-                      {Number(floor) === 4 ? 'Chestnut' : ""}
-                      {Number(floor) === 5 ? 'Faulkner' : ""}
-                      {Number(floor) === 6 ? 'BWH' : ""}
-                    </Text>
-                    <TTSButton text={floorTTS} />
-                  </Group>
-                </Accordion.Control>
-                <Accordion.Panel>
-                  {direction.map((step, idx) => {
-                    const icon = step.Direction.toLowerCase().includes(
-                      'elevator'
-                    ) ? (
-                      <IconStairs size={20} color="#0E3B99" />
-                    ) : step.Direction.toLowerCase().includes('stairs') ? (
-                      <IconStairs size={20} color="#0E3B99" />
-                    ) : step.Direction === 'Straight' ? (
-                      <IconArrowUp size={20} color="#0E3B99" />
-                    ) : step.Direction === 'Left' ? (
-                      <IconArrowLeft size={16} color="#0E3B99" />
-                    ) : (
-                      <IconArrowRight size={16} color="#0E3B99" />
-                    );
+            <Box style={{ overflowY: 'auto' }}>
+                <Accordion
+                    multiple
+                    styles={{
+                        root: {
+                            height: '450px',
+                            overflowY: 'auto',
+                        },
+                        item: {
+                            marginBottom: '0px',
+                        },
+                    }}
+                    defaultValue={[
+                        'floor-1',
+                        'floor-2',
+                        'floor-3',
+                        'floor-4',
+                        'floor-5',
+                        'floor-6',
+                        'floor-7',
+                    ]}
+                >
+                    {Object.entries(directionsByFloor).map(([floor, direction]) => {
+                        const floorTTS = direction.map((step) => {
+                            if (step.Direction.startsWith('Take')) return step.Direction;
+                            if (step.Direction === 'Straight')
+                                return `Continue straight for ${(step.Distance * getScaleFactor()).toFixed(0)} feet.`;
+                            return `Turn ${step.Direction.toLowerCase()}`;
+                        });
+                        return (
+                            <Accordion.Item key={floor} value={`floor-${floor}`}>
+                                <Accordion.Control>
+                                    <Group>
+                                        <Text fw={700} size="md" c="blue.7">
+                                            {/* Stupid ass logic to change the floor bc we didn't do it right the first time*/}
+                                            {/* Changing for Faulkner and Chestnut*/}
+                                            {Number(floor) === 1 ? 'Floor 1' : ''}
+                                            {Number(floor) === 2 ? 'Floor 3' : ''}
+                                            {Number(floor) === 3 ? 'Floor 4' : ''}
+                                            {Number(floor) === 4 ? 'Chestnut' : ''}
+                                            {Number(floor) === 5 ? 'Faulkner' : ''}
+                                            {Number(floor) === 6 ? 'BWH' : ''}
+                                        </Text>
+                                        <TTSButton text={floorTTS} />
+                                    </Group>
+                                </Accordion.Control>
+                                <Accordion.Panel>
+                                    {direction.map((step, idx) => {
+                                        const icon = step.Direction.toLowerCase().includes(
+                                            'elevator'
+                                        ) ? (
+                                            <IconStairs size={20} color="#0E3B99" />
+                                        ) : step.Direction.toLowerCase().includes('stairs') ? (
+                                            <IconStairs size={20} color="#0E3B99" />
+                                        ) : step.Direction === 'Straight' ? (
+                                            <IconArrowUp size={20} color="#0E3B99" />
+                                        ) : step.Direction === 'Left' ? (
+                                            <IconArrowLeft size={16} color="#0E3B99" />
+                                        ) : (
+                                            <IconArrowRight size={16} color="#0E3B99" />
+                                        );
 
-                    const label = step.Direction.startsWith('Take')
-                      ? step.Direction
-                      : step.Direction === 'Straight'
-                        ? `Continue straight for ${(step.Distance * 1.5).toFixed(0)} feet.`
-                        : `Turn ${step.Direction.toLowerCase()}`;
+                                        const label = step.Direction.startsWith('Take')
+                                            ? step.Direction
+                                            : step.Direction === 'Straight'
+                                              ? `Continue straight for ${(step.Distance * 1.5).toFixed(0)} feet.`
+                                              : `Turn ${step.Direction.toLowerCase()}`;
 
-                    return (
-                      <Box key={idx} mb="xs">
-                        <Group align="center">
-                          <Flex direction="row" gap="sm" align="center">
-                            {icon}
-                            <Text size="sm" fw={500} c="secondaryBlues.5">
-                              {label}
-                            </Text>
-                          </Flex>
-                        </Group>
-                        <Divider color="yellowAccent.4" my="xs" />
-                      </Box>
-                    );
-                  })}
-                </Accordion.Panel>
-              </Accordion.Item>
-            );
-          })}
-        </Accordion>
-      </Box>
-    </Box>
-  );
+                                        return (
+                                            <Box key={idx} mb="xs">
+                                                <Group align="center">
+                                                    <Flex direction="row" gap="sm" align="center">
+                                                        {icon}
+                                                        <Text
+                                                            size="sm"
+                                                            fw={500}
+                                                            c="secondaryBlues.5"
+                                                        >
+                                                            {label}
+                                                        </Text>
+                                                    </Flex>
+                                                </Group>
+                                                <Divider color="yellowAccent.4" my="xs" />
+                                            </Box>
+                                        );
+                                    })}
+                                </Accordion.Panel>
+                            </Accordion.Item>
+                        );
+                    })}
+                </Accordion>
+            </Box>
+        </Box>
+    );
 }
